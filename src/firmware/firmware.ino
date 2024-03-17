4be2872df44209eca314d7e70853a94cf380c19b
Foroxon <Foroxon@users.noreply.github.com>
2024-03-17 16:17:19 +0000

    Release 3.10

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -58,7 +58,7 @@
 #include <melody_factory.h>
 #endif
 
-const PROGMEM char* VERSION = "3.9";
+const PROGMEM char* VERSION = "3.10";
 
 struct Settings {
   const char*   apssid                 = "JAAM";
